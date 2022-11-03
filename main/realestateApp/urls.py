4b975cd918928d83d321e75526ceb98f994816a6
mind Versus market <mvmuser@kube-slave1-ubu.mediacarvers.com>
2022-11-02 21:36:53 -0500

    django project

---
+++ main/realestateApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('<str:slug>', views.blog_detailView, name = 'blog_detail')
+]
