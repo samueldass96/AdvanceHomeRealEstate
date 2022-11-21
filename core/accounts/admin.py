0a261421bdc93c75468893e174ae75bff2de495e
sjmarshall07 <70245767+sjmarshall07@users.noreply.github.com>
2022-11-20 22:26:16 -0600

    Admin Listing Page
    Created admin page for listing including all related models and their corresponding admin pages. No UI work was done behind the scenes.

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
+from .models import Listing, Property_Type, Prop_Status, Subdivision
+
 
 # Register your models here.
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('address', 'price')
+    list_filter = ('address', 'price')
+    search_fields = ('address', 'price', 'square_footage')
 
+@admin.register(Property_Type)
+class Property_TypeAdmin(admin.ModelAdmin):
+    list_display = ('prop_type_name', 'prop_type_descr')
+
+@admin.register(Prop_Status)
+class Prop_StatusAdmin(admin.ModelAdmin):
+    list_display = ('prop_status', 'prop_status_descr')
+
+@admin.register(Subdivision)
+class SubdivisionAdmin(admin.ModelAdmin):
+    list_display = ('subdiv_name', 'subdiv_descr')
